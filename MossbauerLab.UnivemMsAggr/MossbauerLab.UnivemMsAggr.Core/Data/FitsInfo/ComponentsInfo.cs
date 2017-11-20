ec4ca3742db5cb03bf55d598fe8fd79c0a2e8247
Michael Ushakov <um.nix.user@gmail.com>
2017-11-20 21:43:26 +0400

    Common spectrum fit info

--- MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.Core/Data/FitsInfo/ComponentsInfo.cs
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.Core/Data/FitsInfo/ComponentsInfo.cs
@@ -5,7 +5,24 @@ using System.Text;
 
 namespace MossbauerLab.UnivemMsAggr.Core.Data.FitsInfo
 {
-    class ComponentsInfo
+    /// <summary>
+    ///    Bases on *_comp_.txt and *_line*.txt file 
+    /// </summary>
+    public class ComponentsInfo
     {
+        public ComponentsInfo()
+        {
+        }
+
+        public ComponentsInfo(Decimal chiSquareValue, Decimal velocityStep, Decimal hyperfimeFieldPerMmS)
+        {
+            ChiSquareValue = chiSquareValue;
+            VelocityStep = velocityStep;
+            HyperfineFieldPerMmS = hyperfimeFieldPerMmS;
+        }
+
+        public Decimal ChiSquareValue { get; set; }
+        public Decimal VelocityStep { get; set; }
+        public Decimal HyperfineFieldPerMmS { get; set; }
     }
 }
