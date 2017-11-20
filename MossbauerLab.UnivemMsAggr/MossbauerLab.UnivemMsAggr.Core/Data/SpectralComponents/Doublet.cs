0d96bd0ee7caca640c1e97fde564c22fadb4d8d0
Michael Ushakov <um.nix.user@gmail.com>
2017-11-20 21:11:33 +0400

    Were added 2 classes for representing doublet and sextet

---
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.Core/Data/SpectralComponents/Doublet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MossbauerLab.UnivemMsAggr.Core.Data.SpectralComponents
+{
+    public class Doublet
+    {
+        public Doublet()
+        {
+        }
+
+        public Doublet(Decimal lineWidth, Decimal lineWidthPrecision,
+                      Decimal isomerShift, Decimal isomerShiftPrecision,
+                      Decimal quadrupolSplitting, Decimal quadrupolSplittingPrecision,
+                      Decimal relativeArea, Decimal relativeAreaPrecision)
+        {
+            LineWidth = lineWidth;
+            LineWidthPrecision = lineWidthPrecision;
+            IsomerShift = isomerShift;
+            IsomerShiftPrecision = isomerShiftPrecision;
+            QuadrupolSplitting = quadrupolSplitting;
+            QuadrupolSplittingPrecision = quadrupolSplittingPrecision;
+            RelativeArea = relativeArea;
+            RelativeAreaPrecision = relativeAreaPrecision;
+        }
+
+        // Hyperfine parameters
+        public Decimal LineWidth { get; set; }
+        public Decimal LineWidthPrecision { get; set; }
+        public Decimal IsomerShift { get; set; }
+        public Decimal IsomerShiftPrecision { get; set; }
+        public Decimal QuadrupolSplitting { get; set; }
+        public Decimal QuadrupolSplittingPrecision { get; set; }
+        // Area
+        public Decimal RelativeArea { get; set; }
+        public Decimal RelativeAreaPrecision { get; set; }
+    }
+}
---
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.Core/Data/SpectralComponents/Sextet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MossbauerLab.UnivemMsAggr.Core.Data.SpectralComponents
+{
+    public class Sextet
+    {
+        public Sextet()
+        {
+        }
+
+        public Sextet(Decimal lineWidth, Decimal lineWidthPrecision,
+                      Decimal isomerShift, Decimal isomerShiftPrecision,
+                      Decimal quadrupolShift, Decimal quadrupolShiftPrecision,
+                      Decimal hyperfineField, Decimal hyperfineFieldPrecision,
+                      Decimal relativeArea, Decimal relativeAreaPrecision)
+        {
+            LineWidth = lineWidth;
+            LineWidthPrecision = lineWidthPrecision;
+            IsomerShift = isomerShift;
+            IsomerShiftPrecision = isomerShiftPrecision;
+            QuadrupolShift = quadrupolShift;
+            QuadrupolShiftPrecision = quadrupolShiftPrecision;
+            HyperfineField = hyperfineField;
+            HyperfineFieldPrecision = hyperfineFieldPrecision;
+            RelativeArea = relativeArea;
+            RelativeAreaPrecision = relativeAreaPrecision;
+        }
+
+        // Hyperfine parameters
+        public Decimal LineWidth { get; set; }
+        public Decimal LineWidthPrecision { get; set; }
+        public Decimal IsomerShift { get; set; }
+        public Decimal IsomerShiftPrecision { get; set; }
+        public Decimal QuadrupolShift { get; set; }
+        public Decimal QuadrupolShiftPrecision { get; set; }
+        public Decimal HyperfineField { get; set; }
+        public Decimal HyperfineFieldPrecision { get; set; }
+        // Area
+        public Decimal RelativeArea { get; set; }
+        public Decimal RelativeAreaPrecision { get; set; }
+    }
+}
