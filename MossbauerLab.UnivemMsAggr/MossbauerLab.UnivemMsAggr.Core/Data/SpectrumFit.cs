4692458991531ded6a2e75f0ba9deada38a096de
Michael Ushakov <um.nix.user@gmail.com>
2017-11-22 00:53:19 +0400

    Developing components processor

---
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.Core/Data/SpectrumFit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using MossbauerLab.UnivemMsAggr.Core.Data.FitsInfo;
+using MossbauerLab.UnivemMsAggr.Core.Data.SpectralComponents;
+
+namespace MossbauerLab.UnivemMsAggr.Core.Data
+{
+    public class SpectrumFit
+    {
+        public SpectrumFit()
+        {
+        }
+
+        public SpectrumFit(IList<Sextet> sextets, IList<Doublet> doublets, ComponentsInfo info)
+        {
+            Sextets = sextets;
+            Doublets = doublets;
+            Info = info;
+        }
+
+        public IList<Sextet> Sextets { get; set; }
+        public IList<Doublet> Doublets { get; set; }
+        public ComponentsInfo Info { get; set; }
+    }
+}
---
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.Core/UnivemMs/FilesProcessor/CompProcessor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using MossbauerLab.UnivemMsAggr.Core.Data;
+using MossbauerLab.UnivemMsAggr.Core.Data.FitsInfo;
+using MossbauerLab.UnivemMsAggr.Core.Data.FitsInfo.CalculatedValues;
+
+namespace MossbauerLab.UnivemMsAggr.Core.UnivemMs.FilesProcessor
+{
+    public static class CompProcessor
+    {
+        public static SpectrumFit Process(String componentsFile)
+        {
+            SpectrumFit fit = new SpectrumFit();
+            if (!File.Exists(Path.GetFullPath(componentsFile)))
+                return null;
+            IList<String> content = File.ReadAllLines(Path.GetFullPath(componentsFile));
+            if (!content.Any(line => String.Equals(line, ComponentsFileSign)))
+                return null;
+            UInt16 cahnnelsNumber = GetValue<UInt16>(content, ChannelsNumberKey, ChannelsNumberPattern);
+            Decimal velocityStep = GetValue<Decimal>(content, VelocityStepKey, VelocityStepLinePattern);
+            Decimal chiSquareValue = GetValue<Decimal>(content, ChiSquareKey,ChiSquareLinePattern);
+            fit.Info = new ComponentsInfo(chiSquareValue, velocityStep, EmpiricCalculations.CalculateHyperfineFieldError(velocityStep), cahnnelsNumber);
+            return fit;
+        }
+
+        private static T GetValue<T>(IList<String> fileContent, String key, String pattern) where T : IConvertible
+        {
+            String selectedLine = null;
+            foreach (String line in fileContent)
+            {
+                if (Regex.Match(line, pattern).Success)
+                {
+                    selectedLine = line;
+                    break;
+                }
+            }
+            if (selectedLine == null)
+                return default(T);
+            Int32 index = selectedLine.IndexOf(key, StringComparison.InvariantCulture);
+            if(index < 0)
+                return default(T);
+            index += key.Length;
+            String residual = selectedLine.Substring(index).Trim();
+            String[] parts = residual.Split(' ');
+            return (T)Convert.ChangeType(parts[0], typeof(T));
+        }
+
+        private const String ComponentsFileSign = "Исп.единицы: Is,G,Qs - мм/с; H - кЭ; A - %; отн.G,A - б/разм.";
+        private const String VelocityStepKey = "Цена канала: ";
+        private const String VelocityStepLinePattern = @"^[\w\W]*" + VelocityStepKey + @"[\w\W]*$";
+        private const String ChiSquareKey = "Xi_2 =";
+        private const String ChiSquareLinePattern = @"^" + ChiSquareKey + @"[\w\W]*$";
+        private const String ChannelsNumberKey = "Точек подгонки (интервалы):";
+        private const String ChannelsNumberPattern = @"^Точек подгонки \(интервалы\):[\w\W]*$";
+    }
+}
