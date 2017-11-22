27992a40df5c65eacc0d5a5eccd4a8012993f07f
Michael Ushakov <um.nix.user@gmail.com>
2017-11-23 01:04:23 +0400

    Starting to sort sextets

--- MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.Core.Tests/UnivemMs/FileProcessor/TestCompProcessor.cs
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.Core.Tests/UnivemMs/FileProcessor/TestCompProcessor.cs
@@ -19,10 +19,12 @@ namespace MossbauerLab.UnivemMsAggr.Core.Tests.UnivemMs.FileProcessor
             SpectrumFit fit = CompProcessor.Process(NickelFerriteNaCompFile);
             Assert.IsNotNull(fit, "Checking that nickel ferrite NA fit is not a null");
             Assert.IsNotNull(fit.Info, "Checking that nickel ferrite NA fit Info is not a null");
+
             Assert.AreEqual(4096, fit.Info.ChannelsNumber, "Checking number of channels");
             Assert.AreEqual(0.006, Decimal.Round(fit.Info.VelocityStep, 3), "Checking velocity step");
             Assert.AreEqual(0.2, Decimal.Round(fit.Info.HyperfineFieldPerMmS, 1), "Checking hyperfine field per mm/s");
             Assert.AreEqual(1.106, fit.Info.ChiSquareValue, "Checking chi squeare values");
+
         }
 
         private const String NickelFerriteNaCompFile = @"..\..\CompFilesExamples\Indian.NiFe2.O4-NA-2-4096_comp.10s-2017-3.txt";
--- MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.Core/UnivemMs/FilesProcessor/CompProcessor.cs
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.Core/UnivemMs/FilesProcessor/CompProcessor.cs
@@ -3,22 +3,30 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using MossbauerLab.UnivemMsAggr.Core.Data;
 using MossbauerLab.UnivemMsAggr.Core.Data.FitsInfo;
 using MossbauerLab.UnivemMsAggr.Core.Data.FitsInfo.CalculatedValues;
+using MossbauerLab.UnivemMsAggr.Core.Data.SpectralComponents;
 
 namespace MossbauerLab.UnivemMsAggr.Core.UnivemMs.FilesProcessor
 {
     public static class CompProcessor
     {
+        public enum SortOrder
+        {
+            Asc,
+            Dsc
+        }
+
         /// <summary>
         ///     These files encoding is CP-1251
         /// </summary>
         /// <param name="componentsFile"></param>
         /// <returns></returns>
-        public static SpectrumFit Process(String componentsFile)
+        public static SpectrumFit Process(String componentsFile, SortOrder order = SortOrder.Dsc)
         {
             SpectrumFit fit = new SpectrumFit();
             if (!File.Exists(Path.GetFullPath(componentsFile)))
@@ -30,6 +38,11 @@ namespace MossbauerLab.UnivemMsAggr.Core.UnivemMs.FilesProcessor
             Decimal velocityStep = GetValue<Decimal>(content, VelocityStepKey, VelocityStepLinePattern);
             Decimal chiSquareValue = GetValue<Decimal>(content, ChiSquareKey,ChiSquareLinePattern);
             fit.Info = new ComponentsInfo(chiSquareValue, velocityStep, EmpiricCalculations.CalculateHyperfineFieldError(velocityStep), cahnnelsNumber);
+            IList<String> componentsStartStr = content.Where(line => Regex.Match(line, ComponentStartPattern).Success).ToList();
+            IList<Int32> indexes = componentsStartStr.Select(line => content.IndexOf(line)).ToList();
+            IList<Int32> sextets = indexes.Where(index => content[index].Contains(SextetKey)).ToList();
+            IList<Int32> doublets = indexes.Where(index => content[index].Contains(DoubletKey)).ToList();
+            fit.Sextets = GetSextets(content, sextets, order);
             return fit;
         }
 
@@ -57,6 +70,57 @@ namespace MossbauerLab.UnivemMsAggr.Core.UnivemMs.FilesProcessor
             return (T)Convert.ChangeType(parts[0], typeof(T), format);
         }
 
+        private static IList<Sextet> GetSextets(IList<String> fileContent, IList<Int32> startPosIndexes, SortOrder order)
+        {
+            IList<Sextet> sextets = new List<Sextet>();
+            while (startPosIndexes.Count > 0)
+            {
+                Int32 index = GetNextSextetIndex(fileContent, startPosIndexes, order);
+                startPosIndexes.Remove(index);
+                // todo : umv : proccess
+            }
+            return sextets;
+        }
+
+        private static Int32 GetNextSextetIndex(IList<String> fileContent, IList<Int32> startPosIndexes, SortOrder order)
+        {
+            const Int32 fieldLineOffset = 3;
+            Decimal extremalField = order == SortOrder.Asc ? 1000000 : 0;
+            Int32 selectedIndex = 0;
+            foreach (Int32 index in startPosIndexes)
+            {
+                String fieldStr = fileContent[index + fieldLineOffset];
+                Tuple<Decimal, Decimal> field = GetValue(fieldStr, HyperfineFieldKey);
+                if ((order == SortOrder.Asc && extremalField > field.Item1 + field.Item2) ||
+                    (order == SortOrder.Dsc && extremalField < field.Item1 + field.Item2))
+                { 
+                    extremalField = field.Item1 + field.Item2;
+                    selectedIndex = index;
+                }
+            }
+            return selectedIndex;
+        }
+
+        private static Tuple<Decimal, Decimal> GetValue(String line, String key)
+        {
+            Int32 startIndex = line.IndexOf(key, StringComparison.InvariantCulture);
+            if (startIndex == -1)
+                throw new InvalidOperationException("Unexpected line format");
+            startIndex += key.Length;
+            String lineResidual = line.Substring(startIndex);
+            Int32 errorStartIndex = lineResidual.IndexOf("(", StringComparison.InvariantCulture);
+            Int32 errorEndIndex = lineResidual.IndexOf(")", StringComparison.InvariantCulture);
+            String errorValue = lineResidual.Substring(errorStartIndex + 1, errorEndIndex - errorStartIndex - 1).Trim();
+            String[] parts = lineResidual.Trim().Split(' ');
+            NumberFormatInfo format = new NumberFormatInfo();
+            format.NumberDecimalSeparator = ".";
+            // ReSharper disable PossibleNullReferenceException
+            Decimal value = (Decimal)Convert.ChangeType(parts[0], typeof(Decimal), format);
+            Decimal error = (Decimal)Convert.ChangeType(errorValue, typeof(Decimal), format);
+            // ReSharper restore PossibleNullReferenceException
+            return  new Tuple<Decimal, Decimal>(value, error);
+        }
+
         private const String CompFilesCodePage = "Windows-1251";
 
         private const String ComponentsFileSign = "Исп.единицы: Is,G,Qs - мм/с; H - кЭ; A - %; отн.G,A - б/разм.";
@@ -66,5 +130,11 @@ namespace MossbauerLab.UnivemMsAggr.Core.UnivemMs.FilesProcessor
         private const String ChiSquareLinePattern = @"^" + ChiSquareKey + @"[\w\W]*$";
         private const String ChannelsNumberKey = "Точек подгонки (интервалы):";
         private const String ChannelsNumberPattern = @"^Точек подгонки \(интервалы\):[\w\W]*$";
+        private const String ComponentStartPattern = @"^\d{1,2}: Имя - [\w\W]*$";
+
+        private const String SextetKey = "Sextet";
+        private const String DoubletKey = "Doublet";
+
+        private const String HyperfineFieldKey = "H =";
     }
 }
