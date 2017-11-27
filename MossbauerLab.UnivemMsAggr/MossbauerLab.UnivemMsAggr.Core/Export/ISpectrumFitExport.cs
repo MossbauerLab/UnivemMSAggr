09613fc5484be6ad1a4b7c4e7801bbd701bd4d6a
Michael Ushakov <um.nix.user@gmail.com>
2017-11-27 13:52:33 +0400

    Was added export interface

---
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.Core/Export/ISpectrumFitExport.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MossbauerLab.UnivemMsAggr.Core.Export
+{
+    public interface ISpectrumFitExport
+    {
+        Boolean Export(String destination);
+    }
+}
---
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.Core/Export/SpectrumFitToTextExport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MossbauerLab.UnivemMsAggr.Core.Export
+{
+    public class SpectrumFitToTextExport : ISpectrumFitExport
+    {
+        public Boolean Export(String destination)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
