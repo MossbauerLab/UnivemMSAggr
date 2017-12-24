57f6513524c7e78d89f31e10b011f869c71d17c5
Unknown <um.nix.user@gmail.com>
2017-12-24 18:42:53 +0400

    On + run new window

--- MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.GUI/Views/MainWindowView.xaml.cs
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.GUI/Views/MainWindowView.xaml.cs
@@ -1,23 +1,30 @@
 ﻿using System;
 using System.Windows;
 using MossbauerLab.UnivemMsAggr.GUI.ViewModels;
+using MossbauerLab.UnivemMsAggr.GUI.Views.Activators;
 
 namespace MossbauerLab.UnivemMsAggr.GUI.Views
 {
     /// <summary>
     /// Interaction logic for MainWindowView.xaml
     /// </summary>
-    public partial class MainWindowView
+    public partial class MainWindowView : Window
     {
         public MainWindowView()
         {
             InitializeComponent();
+            
             DataContext = new MainWindowViewModel();
-
+            AddButton.Click += OnAddItemClick;
             MoveUpButton.MouseMove += OnItemMoveMouseMove;
             MoveDownButton.MouseMove += OnItemMoveMouseMove;
             MoveUpButton.MouseLeftButtonUp += OnItemMoveClick;
             MoveDownButton.MouseLeftButtonUp += OnItemMoveClick;
+        }
+
+        private void OnAddItemClick(Object sender, RoutedEventArgs args)
+        {
+            ViewActivator.Activate<CompSelectView>();
         }
 
         private void OnItemMoveMouseMove(Object sender, RoutedEventArgs args)
