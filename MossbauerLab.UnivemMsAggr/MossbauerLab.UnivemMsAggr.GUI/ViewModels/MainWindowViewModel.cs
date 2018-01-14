8f89cb6c0eac8cc2dd0ed23075d96f37b57455fc
Unknown <um.nix.user@gmail.com>
2018-01-14 23:35:33 +0400

    Some styling

--- MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.GUI/ViewModels/MainWindowViewModel.cs
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.GUI/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows.Input;
 using MossbauerLab.UnivemMsAggr.GUI.Annotations;
 using MossbauerLab.UnivemMsAggr.GUI.Commands;
