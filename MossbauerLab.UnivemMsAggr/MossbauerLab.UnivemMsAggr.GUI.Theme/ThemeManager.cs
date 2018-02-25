d2592ef8a93e37d3f11acc58d10933748295466c
Lunev_EY <edd.layer@gmail.com>
2018-02-25 13:05:54 +0500

    added new project into the solution for theme managing (implemented via combobox) also there are several temporal changes on the mainwindow (button is just for test style and combobox with theme' list)

---
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.GUI.Theme/ThemeManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace MossbauerLab.UnivemMsAggr.GUI.Theme
+{
+
+    /// <summary>
+    /// Theme manager for an WPF application.
+    /// </summary>
+    public static class ThemeManager
+    {
+        private static readonly IDictionary<DispatcherObject, ResourceDictionary> CurrentThemes;
+
+        static ThemeManager()
+        {
+            CurrentThemes = new Dictionary<DispatcherObject, ResourceDictionary>();
+        }
+
+        /// <summary>
+        /// The available themes to be applied to a content control or the whole application.
+        /// </summary>
+        public static IEnumerable<Theme> AvailableThemes { get; set; }
+
+        /// <summary>
+        /// Applies a theme to the application.
+        /// </summary>
+        /// <param name="theme">The theme to be applied.</param>
+        public static void ApplyTheme(Theme theme)
+        {
+            var localThemes = AvailableThemes ?? Enumerable.Empty<Theme>();
+            if (localThemes.All(x => x != theme))
+            {
+                throw new ArgumentException("Unknown theme!");
+            }
+
+            ApplyThemeImpl(Application.Current, Application.Current.Resources, theme);
+        }
+
+        /// <summary>
+        /// Applies a theme to a content control
+        /// </summary>
+        /// <param name="control">The control the theme will be applied to.</param>
+        /// <param name="theme">The theme to be applied</param>
+        public static void ApplyTheme(ContentControl control, Theme theme)
+        {
+            //Contract.Requires<ArgumentNullException>(control != null);
+
+            var localThemes = AvailableThemes ?? Enumerable.Empty<Theme>();
+            if (localThemes.All(x => x != theme))
+            {
+                throw new ArgumentException("Unknown theme!");
+            }
+
+            ApplyThemeImpl(control, control.Resources, theme);
+        }
+
+        private static void ApplyThemeImpl(DispatcherObject @object, ResourceDictionary resources, Theme theme)
+        {
+            //Contract.Requires<ArgumentNullException>(resources != null);
+
+            ResourceDictionary current;
+            if (CurrentThemes.TryGetValue(@object, out current))
+            {
+                resources.MergedDictionaries.Remove(current);
+
+            }
+
+            if (theme != null && theme.Uri != null)
+            {
+                var resourceDictionary = Application.LoadComponent(theme.Uri) as ResourceDictionary;
+                if (resourceDictionary != null)
+                {
+                    resources.MergedDictionaries.Add(resourceDictionary);
+                    CurrentThemes[@object] = resourceDictionary;
+                }
+            }
+        }
+    }
+}
--- MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.GUI/Views/MainWindowView.xaml.cs
+++ MossbauerLab.UnivemMsAggr/MossbauerLab.UnivemMsAggr.GUI/Views/MainWindowView.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Windows;
 using MossbauerLab.UnivemMsAggr.GUI.ViewModels;
 using MossbauerLab.UnivemMsAggr.GUI.Views.Activators;
+using MossbauerLab.UnivemMsAggr.GUI.Theme;
 
 namespace MossbauerLab.UnivemMsAggr.GUI.Views
 {
@@ -13,7 +16,7 @@ namespace MossbauerLab.UnivemMsAggr.GUI.Views
         public MainWindowView()
         {
             InitializeComponent();
-            
+            ThemesControl.ItemsSource = GetThemes();
             DataContext = new MainWindowViewModel();
             AddButton.Click += OnAddItemClick;
         }
@@ -21,6 +24,19 @@ namespace MossbauerLab.UnivemMsAggr.GUI.Views
         private void OnAddItemClick(Object sender, RoutedEventArgs args)
         {
             ViewActivator.Activate<CompSelectView>();
+        }
+
+        private IEnumerable<Theme.Theme> GetThemes()
+        {
+            return new[] {
+                new Theme.Theme("Default theme", new Uri(GetThemeUri("DefaultTheme.xaml"), UriKind.Relative)),
+                new Theme.Theme("Dark theme", new Uri(GetThemeUri("DarkTheme.xaml"), UriKind.Relative))
+            };
+        }
+
+        private String GetThemeUri(String filename)
+        {
+            return String.Concat("/MossbauerLab.UnivemMsAggr.GUI;component/Themes/", filename);
         }
     }
 }
